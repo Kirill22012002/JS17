c283eeb48468b78961f3df9bb9fa070f74e2a82f
AnastasiyaPerepechkina <perepechkina.anastasiya.2002@yandex.ru>
2025-03-18 20:41:20 -0700

    add popup

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,43 @@
-const formRegistration = document.querySelector('.form_registration');
-const formLogin = document.querySelector('.form_login');
+const formRegistration = document.querySelector(".form_registration");
+const formLogin = document.querySelector(".form_login");
 
-const linkToLogin = formRegistration.querySelector('.link_to-login');
-const linkToRegistration = formLogin.querySelector('.link_to-registration');
+const linkToLogin = formRegistration.querySelector(".link_to-login");
+const linkToRegistration = formLogin.querySelector(".link_to-registration");
 
-linkToLogin.addEventListener('click', () => {
-    toogleActiveF(formRegistration, formLogin);
+linkToLogin.addEventListener("click", () => {
+  toogleActiveF(formRegistration, formLogin);
 });
 
-linkToRegistration.addEventListener('click', () => {
-    toogleActiveF(formLogin, formRegistration);
+linkToRegistration.addEventListener("click", () => {
+  toogleActiveF(formLogin, formRegistration);
 });
 
 function toogleActiveF(hide, active) {
-    hide.style.display = 'none';
-    setTimeout(() => {
-        active.style.display = 'flex';
-    }, 150);
-};
+  hide.style.display = "none";
+  setTimeout(() => {
+    active.style.display = "flex";
+  }, 150);
+}
+
+fetch("http://192.168.100.39:5117/api/Ping/PingServer")
+  .then((response) => {
+    if (response.ok) {
+      serverAvailablePopup();
+    } else {
+      serverNotAvailablePopup();
+    }
+  })
+  .catch((err) => {
+    serverNotAvailablePopup();
+    console.log(err);
+  });
+
+function serverAvailablePopup() {
+  const popup = document.querySelector(".popup");
+  popup.style.backgroundColor = "#59981A";
+}
+
+function serverNotAvailablePopup() {
+  const popup = document.querySelector(".popup");
+  popup.style.backgroundColor = "#F51720";
+}
